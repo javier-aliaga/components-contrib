e3b70fc6ca677c5690611f0a4f8c58087eae7850
Javier Aliaga <javier@diagrid.io>
2025-10-27 12:52:42 +0100

    chore: Handle sftp reconnections
    Signed-off-by: Javier Aliaga <javier@diagrid.io>

---
+++ bindings/sftp/sftp_client.go
@@ -0,0 +1,246 @@
+/*
+Copyright 2025 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sftp
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"sync"
+
+	sftpClient "github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+
+	"github.com/dapr/kit/logger"
+)
+
+var log = logger.NewLogger("sftp.client")
+
+type Client struct {
+	sshClient  *ssh.Client
+	sftpClient *sftpClient.Client
+	address    string
+	config     *ssh.ClientConfig
+	lock       sync.RWMutex
+	rLock      sync.Mutex
+}
+
+func newClient(address string, config *ssh.ClientConfig) (*Client, error) {
+	if address == "" || config == nil {
+		return nil, errors.New("sftp binding error: client not initialized")
+	}
+
+	sshClient, err := ssh.Dial("tcp", address, config)
+	if err != nil {
+		return nil, fmt.Errorf("sftp binding error: error create ssh client: %w", err)
+	}
+
+	newSftpClient, err := sftpClient.NewClient(sshClient)
+	if err != nil {
+		_ = sshClient.Close()
+		return nil, fmt.Errorf("sftp binding error: error create sftp client: %w", err)
+	}
+
+	return &Client{
+		sshClient:  sshClient,
+		sftpClient: newSftpClient,
+		address:    address,
+		config:     config,
+	}, nil
+}
+
+func (c *Client) Close() error {
+	_ = c.sshClient.Close()
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.sftpClient.Close()
+}
+
+func (c *Client) list(path string) ([]os.FileInfo, error) {
+	var fi []os.FileInfo
+
+	fn := func() error {
+		var err error
+		c.lock.RLock()
+		defer c.lock.RUnlock()
+		fi, err = c.sftpClient.ReadDir(path)
+		return err
+	}
+
+	err := withReconnection(c, fn)
+	if err != nil {
+		return nil, err
+	}
+
+	return fi, nil
+}
+
+func (c *Client) create(path string) (*sftpClient.File, string, error) {
+	dir, fileName := sftpClient.Split(path)
+
+	var file *sftpClient.File
+
+	createFn := func() error {
+		c.lock.RLock()
+		defer c.lock.RUnlock()
+		cErr := c.sftpClient.MkdirAll(dir)
+		if cErr != nil {
+			return fmt.Errorf("sftp binding error: error create dir %s: %w", dir, cErr)
+		}
+
+		file, cErr = c.sftpClient.Create(path)
+		if cErr != nil {
+			return fmt.Errorf("sftp binding error: error create file %s: %w", path, cErr)
+		}
+
+		return nil
+	}
+
+	rErr := withReconnection(c, createFn)
+	if rErr != nil {
+		return nil, "", rErr
+	}
+
+	return file, fileName, nil
+}
+
+func (c *Client) get(path string) (*sftpClient.File, error) {
+	var f *sftpClient.File
+
+	fn := func() error {
+		var err error
+		c.lock.RLock()
+		defer c.lock.RUnlock()
+		f, err = c.sftpClient.Open(path)
+		return err
+	}
+
+	err := withReconnection(c, fn)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+func (c *Client) delete(path string) error {
+	fn := func() error {
+		var err error
+		c.lock.RLock()
+		defer c.lock.RUnlock()
+		err = c.sftpClient.Remove(path)
+		return err
+	}
+
+	err := withReconnection(c, fn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) ping() error {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	_, err := c.sftpClient.Getwd()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func withReconnection(c *Client, fn func() error) error {
+	log.Debug("withReconnection")
+	err := fn()
+	if err == nil {
+		return nil
+	}
+	log.Debugf("withReconnection: error: %s", err)
+
+	if !shouldReconnect(err) {
+		return err
+	}
+
+	rErr := doReconnect(c)
+	if rErr != nil {
+		return errors.Join(err, rErr)
+	}
+	log.Debug("withReconnection: after reconnect")
+	err = fn()
+	if err != nil {
+		return err
+	}
+
+	log.Debug("withReconnection: success")
+	return nil
+}
+
+func doReconnect(c *Client) error {
+	c.rLock.Lock()
+	defer c.rLock.Unlock()
+
+	err := c.ping()
+	if !shouldReconnect(err) {
+		log.Debug("No reconnection needed - connection is ready")
+		return nil
+	}
+
+	sshClient, err := ssh.Dial("tcp", c.address, c.config)
+	if err != nil {
+		return fmt.Errorf("sftp binding error: error create ssh client: %w", err)
+	}
+
+	newSftpClient, err := sftpClient.NewClient(sshClient)
+	if err != nil {
+		_ = sshClient.Close()
+		return fmt.Errorf("sftp binding error: error create sftp client: %w", err)
+	}
+
+	// Swap under short lock; close old clients after unlocking.
+	c.lock.Lock()
+	oldSftp := c.sftpClient
+	oldSSH := c.sshClient
+	c.sftpClient = newSftpClient
+	c.sshClient = sshClient
+	c.lock.Unlock()
+
+	if oldSftp != nil {
+		_ = oldSftp.Close()
+	}
+	if oldSSH != nil {
+		_ = oldSSH.Close()
+	}
+
+	log.Debug("Reconnected")
+	return nil
+}
+
+// shouldReconnect returns true if the error looks like a transport-level failure
+func shouldReconnect(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	// SFTP status errors that are logical, not connectivity (avoid reconnect)
+	if errors.Is(err, sftpClient.ErrSSHFxPermissionDenied) ||
+		errors.Is(err, sftpClient.ErrSSHFxNoSuchFile) ||
+		errors.Is(err, sftpClient.ErrSSHFxOpUnsupported) {
+		log.Debug("No reconnection needed")
+		return false
+	}
+	log.Debug("Reconnecting...")
+
+	return true
+}
